dc62a716fb91e9c3b309da589d6c516018ad3bfd
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-29 15:08:54 +0100

    databaseAdmin tests passing

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
 import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
 
 public class DatabaseAdminTest {
 
@@ -8,5 +11,16 @@ public class DatabaseAdminTest {
     @Before
     public void before() {
         databaseAdmin = new DatabaseAdmin("Lenny Leonard", "AD 34 56 78 D", 30000.00);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(1.1);
+        assertEquals(33000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(300.00, databaseAdmin.payBonus(), 0.01);
     }
 }
