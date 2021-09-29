4c626238748d08c29efbec0a7404821a72dbf232
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-29 14:44:41 +0100

    Raise salary method added and test passes

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("Mr Smithers", "AB 12 34 56 C", 60000.00, "HR");
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(1.1);
+        assertEquals(66000.00, manager.getSalary(), 0.01);
+    }
 }
