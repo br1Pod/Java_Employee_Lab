3790455e20c1c53065f74ce966d864e6d26c1736
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-29 14:24:27 +0100

    Folder setup done, employeed class and methods created

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,53 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private Double salary;
+
+    public Employee(String name, String niNumber, Double salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNiNumber() {
+        return niNumber;
+    }
+
+    public void setNiNumber(String niNumber) {
+        this.niNumber = niNumber;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+
+    public void raiseSalary(Double raise){
+        this.salary *= raise;
+    }
+
+    public Double payBonus(){
+        return this.salary * 0.01;
+    }
+
+
+
+}
+
+
+
